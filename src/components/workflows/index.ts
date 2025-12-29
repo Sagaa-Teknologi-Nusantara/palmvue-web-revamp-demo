f85a253518d9c89060f8874d8a74771b81f485fa
kevinssheva <kevinssheva@gmail.com>
2025-12-29 15:50:47 +0700

    feat: remove local storage hook

--- src/components/workflows/index.ts
+++ src/components/workflows/index.ts
@@ -1,5 +1,4 @@
 export * from "./StatusBadge";
 export * from "./SubmissionStatusBadge";
-export * from "./WorkflowProgress";
 export * from "./WorkflowSelector";
 export * from "./WorkflowTable";
--- src/hooks/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./useEntities";
-export * from "./useEntityTypes";
-export * from "./useLocalStorage";
-export * from "./useWorkflowRecords";
-export * from "./useWorkflows";
